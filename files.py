0f2481e898b286eac64fd9ed67da2055cde923a7
chalapathyj <chala_shank@yahoo.co.in>
2017-05-24 15:22:35 +0530

    Update files.py

--- files.py
+++ files.py
@@ -13,3 +13,16 @@ print(glob.glob('/home/runner/*.py'))
 metadata = os.stat('file2.py')
 print ("file size:", metadata.st_size)
 print ("file modified:", time.asctime(time.localtime(metadata.st_mtime)))
+
+import re
+
+with open('foo.txt', 'a') as file:
+	file.write('\n added  6th line through append mode')
+	
+with open('foo.txt', encoding='utf-8') as file:
+    for line in file:
+        #srch = re.search('(5th)',line)
+        #sub1 = re.sub('This','It',line, 1)
+        print('{:<1}'.format(line.rstrip()))
+        #if srch:
+          #print (srch.group(1))

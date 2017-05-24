5b2ee0c4686e899695316218964b44308c9f8ee1
chalapathyj <chala_shank@yahoo.co.in>
2017-05-24 15:04:21 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,3 +1,15 @@
 f = open('foo.txt', 'r')
 data = f.read()
 print (data)
+
+import os
+import time
+print(os.getcwd())
+print(os.path.join('/test/', '/myfile'))
+pathname = "/Users/K/dir/subdir/k.py"
+print (os.path.split(pathname))
+import glob
+print(glob.glob('/home/runner/*.py'))
+metadata = os.stat('file2.py')
+print ("file size:", metadata.st_size)
+print ("file modified:", time.asctime(time.localtime(metadata.st_mtime)))
